ca69d31becc8332b6383220d741eda7b0ddd904f
belarminojc <belarmino.cj@sprobe.com>
2026-04-22 11:03:27 +0800

    feat: update home route to point to weather page

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WeatherController;
 
-Route::inertia('/', 'welcome')->name('home');
+Route::inertia('/', 'weather')->name('home');
 Route::inertia('/profile', 'profile')->name('profile');
 
 Route::post('/weather/search', [WeatherController::class, 'search'])->name('weather.search');
 Route::get('/weather/history', [WeatherController::class, 'history'])->name('weather.history');
 Route::delete('/weather/history', [WeatherController::class, 'clearHistory'])->name('weather.clearHistory');
-Route::get('/weather', function () {
-    return inertia('weather');
-})->name('weather.page');
